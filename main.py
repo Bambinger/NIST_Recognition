bb5b92f013c5944eefea42d7e64193dd4876d12d
Bambiner <nico.bambach@hotmail.com>
2021-10-11 17:55:27 +0200

    initial file upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# This is a sample Python script.
+
+# Press Umschalt+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import rename_images
+
+rename_images.main()
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ rename_images.py
@@ -0,0 +1,38 @@
+import os
+from shutil import copyfile
+
+
+def get_class(str):
+    return str.split("\\")[1]
+
+
+def main():
+    by_merge_dir = "C:/gitrepositories/Test/by_merge"
+    output_dir = "C:/gitrepositories/Test/data/"
+
+    #by_merge_dir = "C:\gitrepositories\NIST_Data/by_merge"
+    #output_dir = "C:\gitrepositories\NIST_Data/data/"
+    index = 0
+    class_index = -1
+    counter = 0
+    n_copied = 0
+    classes = []
+    for subdir, dirs, files in os.walk(by_merge_dir):
+        for file in files:
+            if get_class(subdir) not in classes:
+                classes.append(get_class(subdir))
+                class_index += 1
+                index = 0
+            if counter % 8 == 0 and file.endswith(".png"):
+                copyfile(os.path.join(subdir, file),
+                        os.path.join(output_dir, "class_{}_index_{}.png".format(class_index, index)))
+
+                print("Copied " + os.path.join(subdir, file) + " to "
+                      + os.path.join(output_dir, "class_{}_index_{}.png".format(class_index, index)))
+                index += 1
+                n_copied += 1
+            counter += 1
+    print("Total images: " + str(n_copied))
+
+if __name__ == "__main__":
+    main()
